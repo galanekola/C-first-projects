241b84cdc2d606a0ede80bee652cb8038d906703
galanekola <160484876+galanekola@users.noreply.github.com>
2024-04-14 23:19:03 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -153,7 +153,7 @@ void zadanie3_1 ()
     }
 }
 
-void zadanie_sam_1 ()
+void lab2_1 ()//Napisać program wyznaczający n-ty wyraz ciągu Fibonacciego określonego rekurencyjnie: Fn+2=Fn+Fn+1,n≥1, przy czym F1=F2=1. Liczbę n należy pobrać od użytkownika.
 {
     int fib;
     cout<<"Podaj numer wyrazu ktory chcesz wyznaczyc:";
@@ -183,7 +183,7 @@ void zadanie_sam_1 ()
     cout<<fib<<endl;
 }
 
-void zadanie_sam_2 () //Napisać program wyznaczający rozstęp ciągu n liczb całkowitych (rozstęp, to różnica pomiędzy maksimum,
+void lab2_2 () //Napisać program wyznaczający rozstęp ciągu n liczb całkowitych (rozstęp, to różnica pomiędzy maksimum,
 //a minimum ciągu liczb całkowitych), kolejne wyrazy ciągu podaje użytkownik
 //poprawione
 {
@@ -223,7 +223,7 @@ void zadanie_sam_2 () //Napisać program wyznaczający rozstęp ciągu n liczb c
 }
 //#include <ctime>
 //#include <cstdlib>
-void zadanie_sam_4 ()//program który jest grą polegającą na zgadywaniu przez użytkownika liczby wylosowanej przez generator liczb pseudolosowych.
+void lab2_4 ()//program który jest grą polegającą na zgadywaniu przez użytkownika liczby wylosowanej przez generator liczb pseudolosowych.
 // Program losuje liczbę z zakresu od 1 do 100, a następnie pyta użytkownika o nią dopóki on nie poda poprawnej odpowiedzi, po każdej odpowiedzi
 //podając podpowiedź użytkownikowi: za duzo! lub za malo
 //Wskazówka: aby wylosować liczbę pseudolosową ze zbioru {0,1,…,b−1}, gdzie b
@@ -258,6 +258,25 @@ void zadanie_sam_4 ()//program który jest grą polegającą na zgadywaniu przez
 
     }
 }
+void lab2_5 ()//Napisać program wyznaczający przybliżoną wartość całki I=∫10x3dx za pomocą metody trapezów (patrz: Całkowanie numeryczne (wikipedia)). Przedział ⟨0;1⟩ podzielić na n punktów, przy czym liczbę n podaje użytkownik. Wynik obliczeń numerycznych porównać z wynikiem dokładnym I=0,25, tzn. wyznaczyć błąd względny przybliżenia i wyrazić go w procentach (błąd względny, to bezwzględna wartość różnicy przybliżenia i dokładnej wartości podzielona przez dokładną wartość, aby wyrazić go w procentach należy jego wartość pomnożyć przez 100). Czy zwiększenie ilości punktów pośrednich n poprawia wynik? co się dzieje kiedy podamy za duże n (np. n=10000)? Wskazówka: zdefiniować zmienną delta_x=1/(float)n (długość przedziału przez ilość punktów pośrednich) oraz zmienną calka, której wartość początkowo wynosi zero. W pętli for zdefiniować zmienne x1 i x2, które są kolejnymi punktami pośrednimi oraz do aktualnej wartości zmiennej calka dodać pole odpowiedniego trapezu.
+{
+    int przedzial_min=0;
+    int przedzial_max=1;
+    cout<<"Podaj n:"<<endl;
+    float n;
+    cin>>n;
+    float calka =0;
+    float h=(przedzial_max - przedzial_min)/n;
+
+    for (int i=1; i<n; i++)
+    {
+        calka += pow((przedzial_min + i *h),3);
+    }
+    calka += (pow(przedzial_min,3) + pow(przedzial_max,3))/2;
+    calka *=h;
+    cout<<"Wynik calkowania: "<<calka<<endl;
+}
+
 void lab1_1 () //teraz program, który pobiera od użytkownika wymiar w stopach i wyprowadza na ekran wymiar podany w metrach
 //(jedna stopa angielska to 30,48 cm).
 {
@@ -385,7 +404,7 @@ if (xt>0 && yt<0)
 }
 
 
-void lab_3_zad_1 () //Wskaźnik masy ciała BMI
+void lab3_1 () //Wskaźnik masy ciała BMI
 
 {
     string Imie;
@@ -408,7 +427,7 @@ void lab_3_zad_1 () //Wskaźnik masy ciała BMI
 
 }
 
-void lab_3_zad_2 ()//Napisać program, który wyznacza średnią arytmetyczną liczb z przedziału ⟨5,100⟩, które nie są podzielne przez 3 dla ciągu n liczb całkowitych wprowadzonego przez użytkownika z klawiatury (ilość wyrazów ciągu n także podaje użytkownik). Zabezpieczyć program przed sytuacją kiedy takich liczb w wprowadzonym ciągu nie ma (wtedy średnia jest nieokreślona).
+void lab3_2 ()//Napisać program, który wyznacza średnią arytmetyczną liczb z przedziału ⟨5,100⟩, które nie są podzielne przez 3 dla ciągu n liczb całkowitych wprowadzonego przez użytkownika z klawiatury (ilość wyrazów ciągu n także podaje użytkownik). Zabezpieczyć program przed sytuacją kiedy takich liczb w wprowadzonym ciągu nie ma (wtedy średnia jest nieokreślona).
 {
 int x1 = 5;
 float n;
@@ -433,7 +452,7 @@ srednia = suma/k;
 cout<<"Srednia arytmetyczna wynosi: "<<srednia<<endl;
 }
 
-void lab_3_zad_3 () //Napisać program wyznaczający sumę wszystkich dzielników wprowadzonej liczby naturalnej n≥1
+void lab3_3 () //Napisać program wyznaczający sumę wszystkich dzielników wprowadzonej liczby naturalnej n≥1
 {
     cout<<"Wprowadź liczbe której chcesz dzielniki"<<endl;
     int n;
@@ -450,7 +469,7 @@ void lab_3_zad_3 () //Napisać program wyznaczający sumę wszystkich dzielnikó
 
 }
 
-void lab_3_zad_4 ()
+void lab3_4 ()//Napisać program, który wyznacza liczby wystąpień poszczególnych liter alfabetu w słowie wprowadzonym z klawiatury przez użytkownika, przy czym wyświetlane są tylko ilości wystąpień tych liter, które faktycznie występują w danym słowie.
 {
     string slowo;
     cin>>slowo;
@@ -475,7 +494,99 @@ float farenhajty_na_celcjusze (float a)
 {
     return ((((a-32)*5)/9));
 }
-void zadanie4_1 () //Napisać program, który wykorzystuje dwie funkcje: cejsjusze_na_fahrenheity i fahrenheity_na_celsjusze (obie zwracają wartości typu float i mają jeden argument także typu float) do odpowiedzi na pytania:
+void lab3_5() //Zmodyfikować trzeci program (gra Czolgi) w ten sposób aby każdy z czołgów potrzebował trzech trafień aby zostać zniszczony, po każdym trafieniu zwiększyć odległość pomiędzy czołgami losowo w zakresie od 100m do 500m. Oczywiście po każdym trafieniu należy podać informację o nowej odległości, podobnie jak i informację o ilości trafień obu czołgów
+{
+        srand(time(NULL));
+    bool trafiony_gracz_1 = false; //zmienna logiczna: true, jesli gracz 1 zostal trafiony
+    bool trafiony_gracz_2 = false; //zmienna logiczna: true, jesli gracz 2 zostal trafiony
+    int zycie_czolgu_1 = 3;
+    int zycie_czolgu_2 = 3;
+    float w = 4; //szerokosc czolgu w [m]
+    float dx = 2000; //odleglosc miedzy czolgami w [m]
+    float v0; //predkosc poczatkowa pocisku w [m/s]
+    float alpha; //kat nachylenia lufy do poziomu w stopniach
+    int kolejka = 0; //zmienna pozwalajaca wyznaczyc gracza, ktory ma wykonac ruch
+    float wplyw_wiatru; //wplyw wiatru na predkosc poczatkowa pocisku
+    float xpocisku; //miejsce uderzenia pocisku w ziemie
+    string nazwa_gracz_1, nazwa_gracz_2; //nazwy graczy 1 i 2;
+    const float g = 9.81; //przyspieszenie ziemskie
+    const float PI = 3.14; //przyblizenie liczby PI
+
+    cout<<"Nazwa gracza 1: ";
+    cin>>nazwa_gracz_1;
+    cout<<"Nazwa gracza 2: ";
+    cin>>nazwa_gracz_2;
+
+    system("cls");
+
+    while (zycie_czolgu_1 > 0 && zycie_czolgu_2 > 0) {
+          wplyw_wiatru = rand()%9-4; //losuje liczbe ze zbioru {-4,-3,-2,-1,0,1,2,3,4}
+          if (kolejka == 0) {
+             //ruch gracza 1:
+             cout << "Twoje zycie: "<<zycie_czolgu_1<<endl;
+             cout << "Zycie przeciwnika: "<<zycie_czolgu_2<<endl;
+             cout<<nazwa_gracz_1<<" podaj predkosc poczatkowa pocisku (w [m/s]): ";
+             cin>>v0;
+             cout<<nazwa_gracz_1<<" podaj kat nachylenia lufy do poziomu (w stopniach): ";
+             cin>>alpha;
+             xpocisku = v0*v0*sin(2*alpha*PI/180.0)/g;
+             xpocisku += wplyw_wiatru;
+             if (fabs(dx-xpocisku)<= 0.5*w) {
+                int random = rand() % 401 + 100;
+                dx += random;
+                zycie_czolgu_2--;
+                cout<<"TRAFIENIE!"<<endl;
+                cout << "Pozostale zycie przeciwnika: "<<zycie_czolgu_2;
+                system("pause");
+             }
+             else {
+                cout<<"Pudlo! Pocisk wyladowal w odleglosci: "<<xpocisku<<endl;
+                cout<<"Spudlowales o "<<fabs(dx-xpocisku)<<" [m]"<<endl;
+                cout<<"Wiatr zmienil miejsce uderzenia pocisku o "<<wplyw_wiatru<<" [m]"<<endl<<endl;
+             }
+
+             kolejka = 1; //po wykonaniu ruchu przez gracza 1, kolejka przechodzi na gracza 2
+          } else {
+             //ruch gracza 2
+             cout << "Twoje zycie: "<<zycie_czolgu_1<<endl;
+             cout << "Zycie przeciwnika: "<<zycie_czolgu_2<<endl;
+             cout<<nazwa_gracz_2<<" podaj predkosc poczatkowa pocisku (w [m/s]): ";
+             cin>>v0;
+             cout<<nazwa_gracz_2<<" podaj kat nachylenia lufy do poziomu (w stopniach): ";
+             cin>>alpha;
+             xpocisku = v0*v0*sin(2*alpha*PI/180.0)/g;
+             xpocisku += wplyw_wiatru;
+             if (fabs(dx-xpocisku)<= 0.5*w) {
+                int random = rand() % 401 + 100;
+                dx += random;
+                zycie_czolgu_1--;
+                cout<<"TRAFIENIE!"<<endl;
+                cout << "Pozostale zycie przeciwnika: "<<zycie_czolgu_1;
+                system("pause");
+             }
+             else {
+                cout<<"Pudlo! Pocisk wyladowal w odleglosci: "<<xpocisku<<endl;
+                cout<<"Spudlowales o "<<fabs(dx-xpocisku)<<" [m]"<<endl;
+                cout<<"Wiatr zmienil miejsce uderzenia pocisku o "<<wplyw_wiatru<<" [m]"<<endl<<endl;
+             }
+
+             kolejka = 0; //po wykonaniu ruchu przez gracza 2, kolejka przechodzi na gracza 1
+          }
+    }
+
+    system("cls");
+
+    //wyswietlic komunikat o wygranej gracza 1 lub 2
+    if (zycie_czolgu_1 <= 0)
+       cout<<nazwa_gracz_2<<" wygral!"<<endl;
+    else
+       cout<<nazwa_gracz_1<<" wygral!"<<endl;
+
+
+    system("PAUSE");
+}
+
+void lab5_1 () //Napisać program, który wykorzystuje dwie funkcje: cejsjusze_na_fahrenheity i fahrenheity_na_celsjusze (obie zwracają wartości typu float i mają jeden argument także typu float) do odpowiedzi na pytania:
 //W jakiej temperaturze (w skali Fahrenheita) zamarza woda, a w jakiej wrze?
 //Jaka temperatura (w skali Celsjusza) odpowiada temperaturze 0∘F i 100∘F w skali Fahrenheita?
 {
@@ -502,7 +613,7 @@ void zadanie4_1 () //Napisać program, który wykorzystuje dwie funkcje: cejsjus
     }
    }
 }
-void zadanie4_2() //Wykorzystując funkcję czy_pierwsza z przykładowego programu pierwszego, napisać program wyświetlający wszystkie liczby pierwsze z przedziału ⟨2,1000⟩ oraz wyznaczający ich ilość.
+void lab5_2() //Wykorzystując funkcję czy_pierwsza z przykładowego programu pierwszego, napisać program wyświetlający wszystkie liczby pierwsze z przedziału ⟨2,1000⟩ oraz wyznaczający ich ilość.
 {
     int pierwsze = 0;
     for (int i=2; i<=1000; i++)
@@ -527,13 +638,13 @@ void zadanie4_2() //Wykorzystując funkcję czy_pierwsza z przykładowego progra
     cout << "Różnica między ilością a aproksymacją: " << abs(pierwsze - aproksymacja) << endl;
 }
 
-int rekurencja_potega(int x, int n) //Za pomocą funkcji rekurencyjnej potega(int x,int n) wyznaczyć xn (liczby x i n podaje użytkownik, wskazówka: wykorzystaj rekurencyjną definicję potęgi: x0=1 oraz xn=xn−1⋅x dla n≥1
+int lab5_3(int x, int n) //Za pomocą funkcji rekurencyjnej potega(int x,int n) wyznaczyć xn (liczby x i n podaje użytkownik, wskazówka: wykorzystaj rekurencyjną definicję potęgi: x0=1 oraz xn=xn−1⋅x dla n≥1
 {
     if(n == 0)
     {
         return 1;
     }
-    else return rekurencja_potega(x,n-1) * x;
+    else return lab5_3(x,n-1) * x;
 }
 
 void odgadywanie(string haslo,char znak,string &napis,int &ilosc_odslonietych)
@@ -548,7 +659,7 @@ void odgadywanie(string haslo,char znak,string &napis,int &ilosc_odslonietych)
 }
 
 
-void zadanie4_4() //Napisać program, który polega na zgadywaniu przez użytkownika hasła przechowywanego w zmiennej haslo typu string. Warunek zakończenia pętli: zgadnięcie hasła przez użytkownika.
+void lab5_4() //Napisać program, który polega na zgadywaniu przez użytkownika hasła przechowywanego w zmiennej haslo typu string. Warunek zakończenia pętli: zgadnięcie hasła przez użytkownika.
 {
     string haslo = "ola galanek";
     string odgadywanie_hasla;
@@ -611,7 +722,7 @@ void odgadywanie2(string haslo,char znak,string &napis,int &ilosc_odslonietych, 
 }
 
 
-void zadanie4_5() //Napisać program, który polega na zgadywaniu przez użytkownika hasła przechowywanego w zmiennej haslo typu string. Warunek zakończenia pętli: zgadnięcie hasła przez użytkownika.
+void lab5_5() //Napisać program, który polega na zgadywaniu przez użytkownika hasła przechowywanego w zmiennej haslo typu string. Warunek zakończenia pętli: zgadnięcie hasła przez użytkownika.
 {
     int zysk =0;
     string haslo = "ola galanek";
@@ -643,12 +754,224 @@ void zadanie4_5() //Napisać program, który polega na zgadywaniu przez użytkow
   system("pause");
 
 }
+vector<int>oceny;
+void lab6_1 () //Napisać program, który pobiera od użytkownika ilość ocen (nie większą niż 100), następnie wypełnia podanymi przez użytkownika ocenami tablicę, a następnie wyznacza średnią arytmetyczną ocen i ich odchylenie standardowe σ
+{
+    int suma = 0;
+    cout<<"Podaj ilosc ocen"<<endl;
+    int rozmiar;
+    cin>>rozmiar;
+
+    for (int i=0; i<rozmiar; i++)
+    {
+        cout<<"Podaj ocene"<<endl;
+        int ocena;
+        cin>>ocena;
+        oceny.push_back(ocena);
+        suma += ocena;
+
+        if (i>100)
+        {
+            break;
+        }
+    }
+    cout<<"Srednia arytmetyczna wynosi: "<<suma/oceny.size()<<endl;
+int a;
+    for (int i=0; i<oceny.size(); i++)
+    {
+        a += pow((oceny[i] - (suma/oceny.size())),2);
+    }
+    cout<<"Odchylenie standardowe wynosi: "<<sqrt(a/oceny.size())<<endl;
+}
+
+vector<int>fibonaczi;
+void lab6_2()//Napisać program, który wyznacza 20 pierwszych liczb Fibonacciego (definicja: F1=F2=1, Fn=Fn−1+Fn−2, dla n=3,4,…) i przechowuje je w tablicy. A następnie dla i=2,3,…,18 sprawdzić czy zachodzi równość:
+{
+int przedostatni_wyraz = 1;
+int przedprzedostatni_wyraz = 1;
+fibonaczi.push_back(1);
+fibonaczi.push_back(1);
+int fib;
+    for (int i=2; i<21; i++)
+    {
+        fib = przedostatni_wyraz + przedprzedostatni_wyraz;
+        przedprzedostatni_wyraz = przedostatni_wyraz;
+        przedostatni_wyraz = fib;
+        fibonaczi.push_back(fib);
+    }
+    for (int i=0; i<21; i++)
+    {
+        cout<<fibonaczi[i]<<endl;
+    }
+}
+
+bool czy_palindrom (string napis)
+{
+    for (int i=0; i<napis.length(); i++)
+    {
+       if (napis[i]!=napis[napis.length()-1-i])
+           {
+               return (false);
+           }
+    }
+    return (true);
+}
+    //definicje zadeklarowanych funkcji:
+void wektor(float x[], float y[], float v[])
+{
+     for (int i=0;i<3;i++)
+     {
+         v[i] = y[i]-x[i];
+     }
+}
+
+float iloczyn_skalarny(float u[],float v[])
+{
+     return (u[0]*v[0]+u[1]*v[1]+u[2]*v[2]);
+}
+
+void iloczyn_wektorowy(float u[], float v[], float w[])
+{
+     w[0] = u[1]*v[2]-u[2]*v[1];
+     w[1] = -u[0]*v[2]+u[2]*v[1];
+     w[2] = u[0]*v[1]-u[1]*v[2];
+}
+
+float dlugosc(float v[])
+{
+     return sqrt(pow(v[0],2) +pow (v[1],2)+pow(v[2],2));
+}
+void lab6_3()//Napisać program, który dla podanych trzech wierzchołków trójkąta A=(xA,yA,zA), B=(xB,yB,zB), C=(xC,yC,zC) wyznacza: kąty wewnętrzne trójkąta i wyraża je w stopniach (sprawdzamy także czy suma kątów wewnętrznych wynosi 180∘ długości boków trójkąta pole trójkąta
+{
+const float PI = 3.14159; //przyblizenie dziesietne liczby pi
+    //1) definiujemy punkty A, B, C:
+    float A[] = {-1,2.34,5}; //wspolrzedne pierwszego punktu
+    float B[] = {2.6,3.5,-1.3}; //wspolrzedne drugiego punktu
+    float C[] = {0.5,-2.1,1}; //wspolrzedne trzeciego punktu
+
+    //wyznaczamy katy wewnatrzne trojkata:
+    //- kat przy wierzcholku A:
+    float AB[3];
+    wektor(A,B,AB);
+    float AC[3];
+    wektor(A,C,AC);
+    float dAB = dlugosc(AB);
+    float dAC = dlugosc(AC);
+    float katA = 180/PI*acos(iloczyn_skalarny(AB,AC)/(dAB*dAC)); //acos to arccos (arcus cosinus)
+    cout<<"kat przy wierzcholku A wynosi: "<<katA<<" stopni"<<endl;
+    //- kat przy wierzcholku B:
+    float BA[3];
+    wektor (B,A,BA);
+    float BC[3];
+    wektor (B,C,BC);
+    float dBC = dlugosc(BC);
+    float dBA = dlugosc(BA);
+    float katB = 180/PI*acos(iloczyn_skalarny(BA,BC)/(dBA*dBC));
+     cout<<"kat przy wierzcholku B wynosi: "<<katB<<" stopni"<<endl;
+
+    //- kat przy wierzcholku C:
+    float CA[3];
+    wektor (C,A,CA);
+    float CB[3];
+    wektor (C,B,CB);
+    float dCB = dlugosc(CB);
+    float dCA = dlugosc (CA);
+    float katC = 180/PI*acos(iloczyn_skalarny(CA,CB)/(dCA*dCB));
+  cout<<"kat przy wierzcholku C wynosi: "<<katC<<" stopni"<<endl;
+   //wyswietlmy sume katow wewnetrznych trojkata (powinno wyjsc 180):
+   cout<<" Suma katow wewnetrznych trojkata wynosi:" <<katA+katB+katC<<endl;
 
 
+    //wyswietlamy dlugosci bokow trojkata
+    cout<<"Bok AB wynosi: "<<dAB<<endl;
+    cout<<"Bok AC wynosi: "<<dAC<<endl;
+    cout<<"Bok BC wynosi: "<<dBC<<endl;
+
+
+
+    //wyznaczamy pole trojkata ABC:
+    float n[3];
+    iloczyn_wektorowy(AC,AB,n);
+    cout<<"Pole trojkata ABC: "<<dlugosc(n)/2<<endl;
+
+    system("PAUSE");
+}
+
+void lab6_4()//Napisać program, który dla pobranego od użytkownika napisu bez spacji sprawdza czy jest on palindromem czy też nie (palindrom, to wyraz, który czyta się tak samo wspak i normalnie, np. kajak, abba, "kobyła ma mały bok" itp.). Sprawdzanie czy dany napis jest palindronem zaimplementować w funkcji bool czy_palindrom(string napis);
+{
+string napis;
+    cout<<"Podaj wyraz"<<endl;
+    cin>>napis;
+    if (czy_palindrom(napis)== true)
+        cout<<"Wyraz jest palindromem"<<endl;
+    else
+        cout<<"Wyraz nie jest palindromem"<<endl;
+}
+
+vector<int>zywi;
+void lab6_5() //Napisać program rozwiązujący problem Flawiusza dla k=2 (patrz link z Wikipedii). W skrócie sformułowanie problemu Flawiusza jest następujące: w kole stoją osoby ponumerowane od 1 do n (osoby n i 1 są sąsiadami)zabijana jest co druga osoba, aż do momentu kiedy pozostanie tylko jedna dla danej liczby osób n, wyznaczyć numer osoby przeżywającej
+{
+    int k=2;
+    int n;
+    cout<<"Podaj n"<<endl;
+    cin>>n;
+    int nastepny = 0;
+    int nastepny_nastepny = 1;
+
+    for (int i=1; i<=n; i++)
+        zywi.push_back(i);
+
+    bool czy_zostal_ostatni = false;
+
+    while (czy_zostal_ostatni == false)
+    {
+        zywi[nastepny_nastepny]=0;
+        bool wyznacz_nastepny = false;
+        bool wyznacz_nastepny_nastepny = false;
+        int i = nastepny_nastepny;
+        while (wyznacz_nastepny == false)
+        {
+            i+=1;
+            if (i>=zywi.size())
+                i=0;
+            if (zywi[i]!= 0)
+            {
+                nastepny =i;
+                wyznacz_nastepny = true;
+            }
+        }
+
+        i= nastepny;
+        while (wyznacz_nastepny_nastepny ==false)
+        {
+            i+=1;
+            if (i>=zywi.size())
+                i=0;
+            if (zywi[i]!= 0)
+            {
+                nastepny_nastepny =i;
+                wyznacz_nastepny_nastepny = true;
+            }
+        }
+        int a =0;
+        for(int j=0; j<zywi.size(); j++)
+        {
+            if (zywi[j]!= 0)
+                a++;
+        }
+        if (a==1)
+            czy_zostal_ostatni = true;
+    }
+    for(int j=0; j<zywi.size(); j++)
+        {
+            if (zywi[j]!= 0)
+                cout<<"Ostatnia osoba to: "<<zywi[j]<<endl;
+        }
+}
 
 int main()
 {
-    zadanie4_5 ();
+    lab6_5();
     return 0;
 }
 
